7475300efb4296b611d56c690e88a512dd3af858
bburns-ddog <29928783+burnsie7@users.noreply.github.com>
2023-09-26 14:49:38 -0700

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@
 #  value = "ssh -i ~/.ssh/${aws_launch_template.hello_template.key_name}.pem -o IdentitiesOnly=yes ubuntu@${aws_instance.app_server.public_ip}"
 #}
 
-output "hello_app_url" {
-  value = "http://${aws_lb.hello_asg_lb.dns_name}:8000"
+output "dns_url" {
+  value = "http://${aws_route53_record.hello_dns_record.name}"
+}
+
+output "lb_url" {
+  value = "http://${aws_lb.hello_asg_lb.dns_name}"
 }
